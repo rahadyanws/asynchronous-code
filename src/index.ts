2c89fe73b548fc34c21983e0e99c6dbd8cdddb51
rahadyan <rahadyanws@gmail.com>
2025-12-17 11:33:55 +0700

    feat: update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,33 @@
-import { Library } from './Library';
-import { Book } from './models/Book';
-import { Member } from './models/Member';
+import { Library } from "./Library";
+import { Book } from "./models/Book";
+import { Member } from "./models/Member";
 
-const lib = new Library();
-lib.addBook(new Book('b1', 'Java Handbook', 'Rahadyan'));
-lib.addBook(new Book('b2', '1991', 'Widhi'));
-lib.registerMember(new Member('m1', 'Saktiaji'));
+const l = new Library();
+l.addBook(new Book("b1", "Java Handbook", "Rahadyan"));
+l.addBook(new Book("b2", "1991", "Widhi"));
+l.registerMember(new Member("m1", "Saktiaji"));
 
-console.log('1. Search "Java":', lib.findBooks('Java')
-    .map(b => `${b.title} ${b.isAvailable ? 'Avail' : 'Taken'}`));
+console.log(
+  '1. Search "Java":',
+  l
+    .findBooks("Java")
+    .map((b) => `${b.title} ${b.isAvailable ? "Avail" : "Taken"}`)
+);
 
 console.log('2. Borrowing "Java"...');
-lib.borrowBook('m1', 'b1');
-console.log('   Status:', lib.findBooks('Java')
-    .map(b => `${b.title} ${b.isAvailable ? 'Avail' : 'Taken'}`));
+l.borrowBook("m1", "b1");
+console.log(
+  "   Status:",
+  l
+    .findBooks("Java")
+    .map((b) => `${b.title} ${b.isAvailable ? "Avail" : "Taken"}`)
+);
 
 console.log('3. Returning "Java"...');
-lib.returnBook('m1', 'b1');
-console.log('   Status:', lib.findBooks('Java')
-    .map(b => `${b.title} ${b.isAvailable ? 'Avail' : 'Taken'}`));
+l.returnBook("m1", "b1");
+console.log(
+  "   Status:",
+  l
+    .findBooks("Java")
+    .map((b) => `${b.title} ${b.isAvailable ? "Avail" : "Taken"}`)
+);
